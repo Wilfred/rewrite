228d719c2236d489b4d5ebe53a3eb091a0df6a1d
Wilfred Hughes <me@wilfred.me.uk>
2016-08-13 14:37:48 -0400

    Crude string parsing

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,32 @@ use syntax::parse::{self, ParseSess};
 
 
 fn main() {
-    println!("Hello, world!");
-
     // Copied shamelessly from format_input in rustfmt.
     let codemap = Rc::new(CodeMap::new());
     let tty_handler =
         Handler::with_tty_emitter(ColorConfig::Auto, None, true, false, codemap.clone());
     let mut parse_session = ParseSess::with_span_handler(tty_handler, codemap.clone());
+
+    // TODO: distil a simple example for
+    // http://stackoverflow.com/questions/26575443/how-do-i-use-the-rustc-parser-libsyntax
+    let src = "fn foo() { \n let x = 1; \n //bar \n let y = x + 1; }".to_owned();
+
+    println!("source: {}", src);
+    println!("----------");
     
+    let result = parse::parse_crate_from_source_str("stdin".to_owned(), src, Vec::new(), &parse_session);
+
+    match result {
+        Ok(krate) => {
+            println!("crate attrs {:?}", krate.attrs);
+            println!("crate config {:?}", krate.config);
+            println!("crate span {:?}", krate.span);
+            let module = krate.module;
+            println!("module inner {:?}", module.inner);
+            println!("module items {:?}", module.items);
+        }
+        Err(err) => {
+            println!("error {:?}", err);
+        }
+    }
 }
