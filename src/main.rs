b51b6cd2dcee80391616b97da80035fdc73a6523
Wilfred Hughes <me@wilfred.me.uk>
2016-08-14 20:57:26 -0400

    Parse a file specified on the CLI

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 extern crate syntex_syntax as syntax;
 
+use std::env;
 use std::rc::Rc;
+use std::path::{PathBuf};
+
 use syntax::codemap::{CodeMap};
 use syntax::errors::{Handler};
 use syntax::errors::emitter::{ColorConfig};
@@ -8,32 +11,33 @@ use syntax::parse::{self, ParseSess};
 
 
 fn main() {
-    // Copied shamelessly from format_input in rustfmt.
-    let codemap = Rc::new(CodeMap::new());
-    let tty_handler =
-        Handler::with_tty_emitter(ColorConfig::Auto, None, true, false, codemap.clone());
-    let mut parse_session = ParseSess::with_span_handler(tty_handler, codemap.clone());
+    let args: Vec<_> = env::args().collect();
 
-    // TODO: distil a simple example for
-    // http://stackoverflow.com/questions/26575443/how-do-i-use-the-rustc-parser-libsyntax
-    let src = "fn foo() { \n let x = 1; \n //bar \n let y = x + 1; }".to_owned();
+    if args.len() == 2 {
+        // Copied shamelessly from format_input in rustfmt.
+        let codemap = Rc::new(CodeMap::new());
+        let tty_handler =
+            Handler::with_tty_emitter(ColorConfig::Auto, None, true, false, codemap.clone());
+        let parse_session = ParseSess::with_span_handler(tty_handler, codemap.clone());
 
-    println!("source: {}", src);
-    println!("----------");
-    
-    let result = parse::parse_crate_from_source_str("stdin".to_owned(), src, Vec::new(), &parse_session);
+        let path = PathBuf::from(args[1].clone());
+        let result = parse::parse_crate_from_file(&path, Vec::new(), &parse_session);
 
-    match result {
-        Ok(krate) => {
-            println!("crate attrs {:?}", krate.attrs);
-            println!("crate config {:?}", krate.config);
-            println!("crate span {:?}", krate.span);
-            let module = krate.module;
-            println!("module inner {:?}", module.inner);
-            println!("module items {:?}", module.items);
+        match result {
+            Ok(krate) => {
+                println!("crate attrs {:?}", krate.attrs);
+                println!("crate config {:?}", krate.config);
+                println!("crate span {:?}", krate.span);
+                let module = krate.module;
+                println!("module inner {:?}", module.inner);
+                println!("module items {:?}", module.items);
+            }
+            Err(err) => {
+                println!("error {:?}", err);
+            }
         }
-        Err(err) => {
-            println!("error {:?}", err);
-        }
+    } else {
+        println!("You need to specify a .rs file.");
     }
+    
 }
