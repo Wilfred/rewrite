6ded6a88482a930c7e1e507424e7e05d72dd1394
Wilfred Hughes <me@wilfred.me.uk>
2016-08-14 22:36:05 -0400

    Print source code as we walk the tree

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 extern crate syntex_syntax as syntax;
 
 use std::env;
-use std::fs::File;
 use std::rc::Rc;
 use std::path::{Path, PathBuf};
 
 use syntax::ast;
+use syntax::ptr::P;
 use syntax::codemap::CodeMap;
 use syntax::errors::Handler;
 use syntax::errors::emitter::ColorConfig;
 use syntax::parse::{self, ParseSess, PResult};
 
-fn new_parse_session() -> ParseSess {
-    let codemap = Rc::new(CodeMap::new());
+fn new_parse_session(codemap: Rc<CodeMap>) -> ParseSess {
     let tty_handler =
         Handler::with_tty_emitter(ColorConfig::Auto, None, true, false, codemap.clone());
     ParseSess::with_span_handler(tty_handler, codemap.clone())
@@ -23,30 +22,36 @@ fn parse_file<'a>(path: &Path, parse_session: &'a ParseSess) -> PResult<'a, ast:
     parse::parse_crate_from_file(path, Vec::new(), &parse_session)
 }
 
+fn print_all_items(items: &Vec<P<ast::Item>>, codemap: &CodeMap) {
+    for item in items {
+        println!("{}", codemap.span_to_snippet(item.span).unwrap());
+        match item.node {
+            ast::ItemKind::Fn(ref fn_decl, _, _, _, _, ref block) => {
+                println!("fn_decl: {:?}\nblock: {:?}\n\n", fn_decl, block);
+            }
+            ast::ItemKind::Impl(..) => {
+                println!("TODO: impl");
+            }
+            _ => {}
+        }
+    }
+}
+
 fn main() {
     let args: Vec<_> = env::args().collect();
 
     if args.len() == 2 {
         let path = PathBuf::from(args[1].clone());
 
-        let parse_session = new_parse_session();
+        let codemap = Rc::new(CodeMap::new());
+        let parse_session = new_parse_session(codemap.clone());
         let result = parse_file(&path, &parse_session);
 
         match result {
             Ok(krate) => {
                 let module = krate.module;
 
-                for item in module.items {
-                    match item.node {
-                        ast::ItemKind::Fn(ref fn_decl, _, _, _, _, ref block) => {
-                            println!("fn_decl: {:?}\nblock: {:?}\n\n", fn_decl, block);
-                        }
-                        ast::ItemKind::Impl(..) => {
-                            println!("TODO: impl");
-                        }
-                        _ => {}
-                    }
-                }
+                print_all_items(&module.items, &codemap)
             }
             Err(err) => {
                 println!("error {:?}", err);
